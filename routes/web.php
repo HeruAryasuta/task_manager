dadc8b4f625ba75333173e0ca31ea7047828742c
Heru Aryasuta <106472221+HeruAryasuta@users.noreply.github.com>
2025-11-24 16:11:29 +0700

    Change home route view to auth.login

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\WeatherController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 })->name('home');
 
 // Authentication Routes
